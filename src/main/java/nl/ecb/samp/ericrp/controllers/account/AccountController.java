8e1c10969a1df2e7adcb57dbb037f641dbfc37c6
Eric Bonestroo <ec.bonestroo@gmail.com>
2012-12-21 15:03:49 +0100

    added some javadoc

--- src/main/java/nl/ecb/samp/ericrp/controllers/account/AccountController.java
+++ src/main/java/nl/ecb/samp/ericrp/controllers/account/AccountController.java
@@ -12,11 +12,21 @@ import nl.ecb.samp.ericrp.persistance.MysqlAdapter;
 
 public class AccountController {
 	private AccountStore store;
-
+	/**
+	 * This is the constructor for the account controller.
+	 */
 	public AccountController()
 	{
 		this.store = AccountStore.getInstance();
 	}
+	/**
+	 * 
+	 * @param p the player thats making a attempt to login.
+	 * @param username username of the account youre trying to load.
+	 * @param password the password of the account youre trying to load
+	 * @throws AccountNotFoundException when no acount is found using the credentials
+	 * @throws playeridAlreadyLoggedInException when the platyer is already logged in
+	 */
 	protected void login(Player p,String username, String password) throws AccountNotFoundException, playeridAlreadyLoggedInException {
 		store.setAccount(p, MysqlAdapter.getInstance().getAccount(username,password));
 	}
