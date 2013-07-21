58710583c983d4032ed931f64daa0fc2d43e6290
Eric Bonestroo <e.bonestroo@gmail.com>
2013-07-21 23:50:22 +0200

    BUILDING: get set add remove entrances

--- src/main/java/nl/ecb/samp/ericrp/building/model/AbstractBuilding.java
+++ src/main/java/nl/ecb/samp/ericrp/building/model/AbstractBuilding.java
@@ -6,7 +6,24 @@ import net.gtaun.shoebill.object.Player;
 
 public abstract class AbstractBuilding {
 	private List<Entrance> entrances;
-	
+
 	public abstract boolean attemptToEnter(Player p);
+
 	public abstract boolean attemptToExit(Player p);
+
+	public List<Entrance> getEntrances() {
+		return entrances;
+	}
+
+	public void setEntrances(List<Entrance> entrances) {
+		this.entrances = entrances;
+	}
+
+	public void addEntrance(Entrance e) {
+		entrances.add(e);
+	}
+
+	public void removeEntrance(Entrance e) {
+		entrances.remove(e);
+	}
 }
